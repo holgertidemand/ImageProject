750e8562e5cc3f387284f3e7ea208d5ff70ab2a0
Holger_Tidemand <tidemand.holger@gmail.com>
2018-05-02 16:23:11 +0200

    resized image

--- src/components/ShowImage.js
+++ src/components/ShowImage.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
-import { Image, View } from 'react-native';
+import { Image, View, StyleSheet } from 'react-native';
 
-class ShowImage extends Component {
-  
-
+class ShowImage extends Component {  
   render() {
+    const { image } = styles;
     const imagePath = '../../assets/victory.jpg';
     return (
       <View>
-        <Image source={ require(imagePath) } />
+        <Image style={ image } source={ require(imagePath) } />
       </View>
     );
   }
 }
 
-
+const styles = StyleSheet.create({
+  image: {
+    height: 250,
+    width: 250
+  }
+})
 
 export default ShowImage;
